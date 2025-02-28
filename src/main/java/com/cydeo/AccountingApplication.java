811c1f76017b010a4d640e5d5a1be079e228f3e3
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2025-02-28 10:11:49 -0500

    Initialized the older version

--- src/main/java/com/cydeo/AccountingApplication.java
+++ src/main/java/com/cydeo/AccountingApplication.java
@@ -1,30 +1,23 @@
 package com.cydeo;
 
-
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableScheduling // Enable Scheduling in our Spring Boot application.
-@EnableAsync // Enable asynchronous execution in our Spring Boot application.
 public class AccountingApplication {
-
-
 
     public static void main(String[] args) {
         SpringApplication.run(AccountingApplication.class, args);
     }
 
     @Bean
-    public ModelMapper modelMapper() {
+    public ModelMapper modelMapper(){
         return new ModelMapper();
     }
+
 }
-
-
